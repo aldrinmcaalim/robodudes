5435420a4d6edb20351607b957833cd8bddaea84
Aldrin <aldrinmcaalim@gmail.com>
2020-10-01 18:55:00 -0400

    Moved node_modules from robodudes
    It is now in robofriends folder just in case I need it

--- src/containers/App.js
+++ src/containers/App.js
@@ -35,7 +35,7 @@ class App extends Component {
       <h1>Loading</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+        <h1 className="f1">RoboDudes</h1>
         <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
             <ErrorBoundary>
